9303b3ff4c98cc7bbc7bd45ab86f9daf18169659
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2023-02-09 16:57:13 +0100

    adds module for common definitions

---
+++ src/dvc_stage/common.py
@@ -0,0 +1,15 @@
+# -*- time-stamp-pattern: "changed[\s]+:[\s]+%%$"; -*-
+# AUTHOR INFORMATION ##########################################################
+# file    : common.py
+# author  : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
+#
+# created : 2023-02-09 16:21:47 (Marcel Arpogaus)
+# changed : 2021-03-26 11:48:25 (Marcel Arpogaus)
+# DESCRIPTION #################################################################
+# ...
+# LICENSE #####################################################################
+# ...
+###############################################################################
+
+# GLOBAL VARIABLES ############################################################
+CUSTOM_MODULE_PREFIX = "import!"
