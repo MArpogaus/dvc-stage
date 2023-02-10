aa636ba4ffbb5721c1e4f27a6a7dc2a7098fc4b3
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2023-02-10 15:47:27 +0100

    allow support for custom loading functions and variables in params

--- src/dvc_stage/loading.py
+++ src/dvc_stage/loading.py
@@ -4,14 +4,15 @@
 # author  : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 #
 # created : 2022-11-15 08:02:51 (Marcel Arpogaus)
-# changed : 2023-02-08 11:44:33 (Marcel Arpogaus)
+# changed : 2023-02-10 15:47:23 (Marcel Arpogaus)
 # DESCRIPTION #################################################################
 # ...
 # LICENSE #####################################################################
 # ...
 ###############################################################################
 # REQUIRED MODULES ############################################################
-import glob
+import fnmatch
+import importlib
 import logging
 import os
 
@@ -19,24 +20,20 @@ import pandas as pd
 from tqdm import tqdm
 
 
-# PUBLIC FUNCTIONS ############################################################
-def get_deps(path):
-    deps = []
-    if isinstance(path, list):
-        for p in path:
-            deps += get_deps(p)
-        deps = list(sorted(set(deps)))
+# PRIVATE FUNCTIONS ###########################################################
+def _get_loading_function(format, import_from):
+    if format == "custom":
+        module_name, function_name = import_from.rsplit(".", 1)
+        fn = getattr(importlib.import_module(module_name), function_name)
+    elif hasattr(pd, format):
+        fn = getattr(pd, "read_" + format)
     else:
-        deps = glob.glob(path)
-
-    assert (
-        len(deps) > 0
-    ), f'Dependencies not found for path "{path}".\nIs DVC Pipeline up to date?'
-    return deps
+        raise ValueError(f'loading function for format "{format}" not found')
+    return fn
 
 
-def load_data(format, path, as_dict=False, **kwds):
-    path = get_deps(path)
+# PUBLIC FUNCTIONS ############################################################
+def load_data(format, path, as_dict=False, key_map=None, import_from=None, **kwds):
     if len(path) == 1:
         path = path[0]
     if isinstance(path, list):
@@ -44,19 +41,28 @@ def load_data(format, path, as_dict=False, **kwds):
         if as_dict:
             data = {}
             for p in tqdm(path):
-                k = os.path.basename(p)
-                k = os.path.splitext(k)[0]
+                k = None
+                if key_map:
+                    for pat, key in key_map.items():
+                        match = fnmatch.fnmatch(p, pat)
+                        if match:
+                            k = key
+                            print(k)
+                            break
+                    if k is None:
+                        raise ValueError(f"Could not find matching key for '{pat}'")
+                else:
+                    k = os.path.basename(p)
+                    k = os.path.splitext(k)[0]
                 data[k] = load_data(
-                    format=format,
-                    path=p,
+                    format=format, path=p, as_dict=as_dict, import_from=import_from
                 )
         else:
             data = []
             for p in tqdm(path):
                 data.append(
                     load_data(
-                        format=format,
-                        path=p,
+                        format=format, path=p, as_dict=as_dict, import_from=import_from
                     )
                 )
         return data
@@ -65,4 +71,5 @@ def load_data(format, path, as_dict=False, **kwds):
             return None
         else:
             logging.info(f"loading data from {path}")
-            return getattr(pd, "read_" + format)(path, **kwds)
+            fn = _get_loading_function(format, import_from)
+            return fn(path, **kwds)
--- src/dvc_stage/utils.py
+++ src/dvc_stage/utils.py
@@ -4,23 +4,29 @@
 # author  : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 #
 # created : 2022-11-15 08:02:51 (Marcel Arpogaus)
-# changed : 2023-02-09 15:51:34 (Marcel Arpogaus)
+# changed : 2023-02-10 13:13:15 (Marcel Arpogaus)
 # DESCRIPTION #################################################################
 # ...
 # LICENSE #####################################################################
 # ...
 ###############################################################################
 # REQUIRED MODULES ############################################################
+import glob
 import logging
+import re
+from typing import Dict
 
 import dvc.api
 import yaml
 
 import dvc_stage
-from dvc_stage.loading import get_deps, load_data
+from dvc_stage.loading import load_data
 from dvc_stage.transforming import apply_transformations
 from dvc_stage.validating import apply_validations
 from dvc_stage.writing import get_outs, write_data
+
+# MODULE GLOBAL VARIABLES #####################################################
+__LOGGER__ = logging.getLogger(__name__)
 
 
 # PRIVATE FUNCTIONS ###########################################################
@@ -36,60 +42,106 @@ def _flatten_dict(d, parent_key="", sep="."):
 
 
 def _load_dvc_yaml():
-    logging.debug("loading dvc.yaml")
+    __LOGGER__.debug("loading dvc.yaml")
     with open("dvc.yaml", "r") as f:
         dvc_yaml = yaml.safe_load(f)
-    logging.debug(dvc_yaml)
+    __LOGGER__.debug(dvc_yaml)
     return dvc_yaml
 
 
 def _check_dvc_yaml(stage):
     dvc_yaml = _load_dvc_yaml()["stages"][stage]
-    logging.debug(f"dvc.yaml:\n{yaml.dump(dvc_yaml)}")
+    __LOGGER__.debug(f"dvc.yaml:\n{yaml.dump(dvc_yaml)}")
     config = _get_dvc_config(stage)["stages"][stage]
     if stage in dvc_yaml["cmd"]:
         config["cmd"] = dvc_yaml["cmd"]
-    logging.debug(f"expected:\n{yaml.dump(config)}")
+    __LOGGER__.debug(f"expected:\n{yaml.dump(config)}")
 
     return dvc_yaml == config
 
 
 def _validate_dvc_yaml(stage):
-    logging.debug("validating dvc.yaml")
+    __LOGGER__.debug("validating dvc.yaml")
     assert _check_dvc_yaml(stage), f"dvc.yaml for {stage} is invalid."
 
 
+def _parse_path(path, params) -> Dict:
+    print(path, params)
+    pattern = re.compile(r"\${([a-z]+)}")  # noqa: W605
+    matches = set(re.findall(pattern, path))
+    print(matches)
+    for g in matches:
+        path = path.replace("${" + g + "}", params[g])
+    print(path)
+    return path, matches
+
+
+def _get_deps(path, params):
+    deps = []
+    param_keys = set()
+    if isinstance(path, list):
+        for p in path:
+            rdeps, rparam_keys = _get_deps(p, params)
+            deps += rdeps
+            param_keys |= rparam_keys
+    else:
+        path, matches = _parse_path(path, params)
+        param_keys |= matches
+        deps = glob.glob(path)
+
+    deps = list(sorted(set(deps)))
+
+    assert (
+        len(deps) > 0
+    ), f'Dependencies not found for path "{path}".\nIs DVC Pipeline up to date?'
+
+    return deps, param_keys
+
+
+def _get_params(stage, all=False):
+    params = dvc.api.params_show(stages=None if all else stage)
+    stage_params = params[stage]
+    global_params = dict(
+        filter(lambda kv: isinstance(kv[1], (int, float, str)), params.items())
+    )
+    __LOGGER__.debug(stage_params, global_params)
+
+    return stage_params, global_params
+
+
 def _get_dvc_config(stage):
-    logging.debug(f"tracing dvc stage: {stage}")
-    params = dvc.api.params_show()[stage]
-    logging.debug(params)
+    __LOGGER__.debug(f"tracing dvc stage: {stage}")
 
-    deps = get_deps(params["load"]["path"])
+    stage_params, global_params = _get_params(stage, all=all)
 
-    config = params.get("extra_stage_fields", {})
+    dvc_params = list(_flatten_dict(stage_params, parent_key=stage).keys())
+    deps, param_keys = _get_deps(stage_params["load"].pop("path"), global_params)
+    dvc_params += list(param_keys)
+
+    config = stage_params.get("extra_stage_fields", {})
     config.update(
         {
             "cmd": f"dvc-stage run {stage}",
-            "deps": deps + params.get("extra_deps", []),
-            "params": list(_flatten_dict(params, parent_key=stage).keys()),
+            "deps": deps + stage_params.get("extra_deps", []),
+            "params": list(sorted(dvc_params)),
             "meta": {"dvc-stage-version": dvc_stage.__version__},
         }
     )
 
-    transformations = params.get("transformations", None)
-    write = params.get("write", None)
-    load = params["load"]
+    transformations = stage_params.get("transformations", None)
+    write = stage_params.get("write", None)
+    load = stage_params["load"]
 
     # if the format is None data loading is skipped and None is returned tracing
     load["format"] = None
 
-    data = load_data(**load)
+    data = load_data(path=deps, **load)
 
     if transformations is not None:
         assert write is not None, "No writer configured."
         data = apply_transformations(data, transformations)
         outs = get_outs(data, **write)
-        config["outs"] = outs + params.get("extra_outs", [])
+        config["outs"] = outs + stage_params.get("extra_outs", [])
 
     config = {"stages": {stage: config}}
 
@@ -104,26 +156,26 @@ def print_stage_config(stage):
 
 def update_dvc_stage(stage):
     if _check_dvc_yaml(stage):
-        logging.info(f"stage definition of {stage} is up to date")
+        __LOGGER__.info(f"stage definition of {stage} is up to date")
     else:
-        logging.info("updating dvc.yaml")
+        __LOGGER__.info("updating dvc.yaml")
         dvc_yaml = _load_dvc_yaml()
         config = _get_dvc_config(stage)["stages"][stage]
         if stage in dvc_yaml["stages"][stage]["cmd"]:
             config["cmd"] = dvc_yaml["stages"][stage]["cmd"]
 
-        logging.info(f"before:\n{yaml.dump(dvc_yaml['stages'][stage])}")
-        logging.info(f"after update:\n{yaml.dump(config)}")
-        logging.warn("This will alter your dvc.yaml")
+        __LOGGER__.info(f"before:\n{yaml.dump(dvc_yaml['stages'][stage])}")
+        __LOGGER__.info(f"after update:\n{yaml.dump(config)}")
+        __LOGGER__.warn("This will alter your dvc.yaml")
         answer = input("type [y]es to continue: ")
 
         if answer.lower() in ["y", "yes"]:
             dvc_yaml["stages"][stage] = config
             with open("dvc.yaml", "w") as f:
                 yaml.dump(dvc_yaml, f, sort_keys=False)
-            logging.info("dvc.yaml successfully updated")
+            __LOGGER__.info("dvc.yaml successfully updated")
         else:
-            logging.error("Operation canceled by user")
+            __LOGGER__.error("Operation canceled by user")
             exit(1)
 
 
@@ -137,16 +189,19 @@ def update_dvc_yaml():
 def run_stage(stage, validate=True):
     if validate:
         _validate_dvc_yaml(stage)
-    params = dvc.api.params_show(stages=stage)[stage]
-    logging.debug(params)
 
+    stage_params, global_params = _get_params(stage)
+    __LOGGER__.debug(stage_params)
+
+    deps, _ = _get_deps(stage_params["load"].pop("path"), global_params)
     data = load_data(
-        **params["load"],
+        path=deps,
+        **stage_params["load"],
     )
 
-    transformations = params.get("transformations", None)
-    validations = params.get("validations", None)
-    write = params.get("write", None)
+    transformations = stage_params.get("transformations", None)
+    validations = stage_params.get("validations", None)
+    write = stage_params.get("write", None)
 
     if transformations is not None:
         assert write is not None, "No writer configured."
@@ -158,5 +213,5 @@ def run_stage(stage, validate=True):
     if write is not None:
         write_data(
             data=data,
-            **params["write"],
+            **stage_params["write"],
         )
